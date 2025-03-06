0402e313ef92473956a3abfe049821c06e6ab6cd
Bruno Martins <bgcngm@gmail.com>
2025-03-06 21:42:19 +0000

    oneplus2: Add IMS libs symlinks

--- oneplus2/oneplus2-vendor.mk
+++ oneplus2/oneplus2-vendor.mk
@@ -419,3 +419,7 @@ PRODUCT_PACKAGES += \
     qcnvitems \
     qcrilhook \
     rcsimssettings
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
